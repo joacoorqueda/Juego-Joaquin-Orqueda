da874f9b522b08f61703df6c8a943a3f5d40e3bc
joacoorqueda <101809787+joacoorqueda@users.noreply.github.com>
2022-06-30 22:02:50 -0300

    commit escalope

---
+++ Assets/Scripts/Token.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Token : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Inventario inventario;
+
+
+    void Start()
+    {
+        inventario = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventario>();
+    }
+
+
+       void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.name == "Player")
+            {
+                inventario.Cantidad = inventario.Cantidad + 1;
+                Destroy(gameObject);
+            }
+
+        }
+
+
+    }
